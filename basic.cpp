08ab8899d9e38db02b168c8c8c46572561ca6e51
chellegrace855 <121882484+chellegrace855@users.noreply.github.com>
2023-01-05 16:33:31 +0800

    Add files via upload

--- basic.cpp
+++ basic.cpp
@@ -244,10 +244,210 @@ void dijkstra(int sz, int** graph, int src)
     }
     
 }
+
+/*/////////////////////////////////////////MAX HEAP///////////////////////*/
+void max_heap(int *a, int m, int n) {
+   int j, t;
+   t = a[m];
+   j = 2 * m;
+   while (j <= n) {
+      if (j < n && a[j+1] > a[j])
+         j = j + 1;
+      if (t > a[j])
+         break;
+      else if (t <= a[j]) {
+         a[j / 2] = a[j];
+         j = 2 * j;
+      }
+   }
+   a[j/2] = t;
+   return;
+}
+void build_maxheap(int *a,int n) {
+   int k;
+   for(k = n/2; k >= 1; k--) {
+      max_heap(a,k,n);
+   }
+}
+/*////////////////////////////BICYCLE ID CLASS////////////////////////////*/
+/*//////////////////////////AVL TREE THINGY///////////////////////////////*/
+class BicType
+{
+    public:
+    int key;
+    BicType *left;
+    BicType *right;
+    int height;
+};
+ 
+// A utility function to get the
+// height of the tree
+int height(BicType *N)
+{
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+ 
+// A utility function to get maximum
+// of two integers
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+ 
+/* Helper function that allocates a
+   new node with the given key and
+   NULL left and right pointers. */
+BicType* newBicType(int key)
+{
+    BicType* node = new BicType();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1; // new node is initially
+                      // added at leaf
+    return(node);
+}
+ 
+// A utility function to right
+// rotate subtree rooted with y
+// See the diagram given above.
+BicType *rightRotate(BicType *y)
+{
+    BicType *x = y->left;
+    BicType *T2 = x->right;
+ 
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+ 
+    // Update heights
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    x->height = max(height(x->left),
+                    height(x->right)) + 1;
+ 
+    // Return new root
+    return x;
+}
+ 
+// A utility function to left
+// rotate subtree rooted with x
+// See the diagram given above.
+BicType *leftRotate(BicType *x)
+{
+    BicType *y = x->right;
+    BicType *T2 = y->left;
+ 
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+ 
+    // Update heights
+    x->height = max(height(x->left),
+                    height(x->right)) + 1;
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+ 
+    // Return new root
+    return y;
+}
+ 
+// Get Balance factor of node N
+int getBalance(BicType *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+ 
+// Recursive function to insert a key
+// in the subtree rooted with node and
+// returns the new root of the subtree.
+BicType* insert(BicType* node, int key)
+{
+    /* 1. Perform the normal BST insertion */
+    if (node == NULL)
+        return(newBicType(key));
+ 
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else // Equal keys are not allowed in BST
+        return node;
+ 
+    /* 2. Update height of this ancestor node */
+    node->height = 1 + max(height(node->left),
+                        height(node->right));
+ 
+    /* 3. Get the balance factor of this ancestor
+        node to check whether this node became
+        unbalanced */
+    int balance = getBalance(node);
+ 
+    // If this node becomes unbalanced, then
+    // there are 4 cases
+ 
+    // Left Left Case
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+ 
+    // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+ 
+    // Left Right Case
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+ 
+    // Right Left Case
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+ 
+    /* return the (unchanged) node pointer */
+    return node;
+}
+void preOrder(BicType *root)
+{
+    if(root != NULL)
+    {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+ 
+ 
 /*//////////////////////////THE STATION CLASS/////////////////////////////*/
 
+
 class Station{
-    bool shortestPath;
+public:
+    bool shortestPath; //ud pernah di dijkstra blm
+    int numBicType;//number of bicycle type
+    BicType* root; //bikin avl tree
+    
+    Station(){
+        root=NULL;
+        numBicType=0;
+        shortestPath=false;
+    }
+    void insertBike(int type){
+        root=insert(root,type);
+        numBicType++;
+    }
+    void output(){
+        preOrder(root);
+    }
+    
 };
 /*//////////////////////////THE SORTING FUNCTION/////////////////////////////*/
 
@@ -324,33 +524,7 @@ void basic(string selectedCase){
     double depreciationConst;
     int rentalCountLim;
     
-    
-    if(bikeInfo.is_open()){ //bike info nya, jd harga, sm depreciation const sama rental count lim
-        
-  
-        bikeInfo>>depreciationConst;
-        bikeInfo>>rentalCountLim;
-        cout<<"rentalCount: "<<rentalCountLim<<endl;
-        cout<<"depreciationConst: "<<depreciationConst<<endl;
-        while(getline(bikeInfo,line)){
-            stringstream stream;
-            stream<<line;
-            cout<<line<<endl;
-        }
-        bikeInfo.close();
-    }
-    if(bike.is_open()){ //tiap bike id nya buat tiap bike type ada di station mana 
-        //B0 0 S1 10 0
-        while(getline(bike,line)){
-            stringstream stream;
-            stream<<line;
-            cout<<line<<endl;
-            stream>>temp1>>a>>temp2>>doubleTemp>>b;
-            //cout<<temp1<<" "<<a<<" "<<temp2<<" "<<doubleTemp<<" "<<b<<endl;
-            
-        }
-        bike.close();
-    }
+    Station* listStation;
     
     if(map.is_open()) //mapping
     {
@@ -395,6 +569,54 @@ void basic(string selectedCase){
 
 map.close();
     } else cout<< "unable to open"<<endl<<endl;
+    
+    listStation = new Station[col];
+    
+    
+    for(int i=0;i<col;i++){
+        Station temp;
+        listStation[i]=temp;
+    }
+    
+    if(bikeInfo.is_open()){ //bike info nya, jd harga, sm depreciation const sama rental count lim
+        
+  
+        bikeInfo>>depreciationConst;
+        bikeInfo>>rentalCountLim;
+        cout<<"rentalCount: "<<rentalCountLim<<endl;
+        cout<<"depreciationConst: "<<depreciationConst<<endl;
+        while(getline(bikeInfo,line)){
+            stringstream stream;
+            stream<<line;
+            cout<<line<<endl;
+        }
+        bikeInfo.close();
+    }
+    
+    if(bike.is_open()){ //tiap bike id nya buat tiap bike type ada di station mana
+        //B0 0 S1 10 0
+        int station, bikeType;
+        while(getline(bike,line)){
+            stringstream stream;
+            stream<<line;
+            cout<<line<<endl;
+            stream>>temp1>>a>>temp2>>doubleTemp>>b;
+            station = stoi(temp2.substr(1));
+            bikeType = stoi(temp1.substr(1));
+            //cout<<listStation[station].numBicType;
+            listStation[station].insertBike(bikeType);
+            
+            
+        }
+        cout<<endl<<endl;
+       for(int i=0;i<col;i++){
+            listStation[i].output();
+           cout<<endl;
+        }
+        bike.close();
+    }
+    
+    
     cout<<endl;
     cout<<"before dijkstra"<<endl;
     for (int i=0; i<col;i++){
